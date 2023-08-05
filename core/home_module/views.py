987628b4d9097c677ff3575f4eb3c6368ff09ee1
budeikin <budeikin52@gmail.com>
2023-08-05 12:07:00 +0330

    set local_setting and add home_page

---
+++ core/home_module/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+
+# Create your views here.
+
+def home_page(request):
+    return HttpResponse('This is Home Page')
