ae2ef8dffb422b4891b96a2db4627defe7d87558
budeikin <budeikin52@gmail.com>
2023-08-10 15:01:36 +0330

    adding product detail page and filter by category

--- core/product_module/urls.py
+++ core/product_module/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import all_products
+from .views import all_products,detail_product
 
 app_name = 'product_module'
 
 urlpatterns = [
-    path('', all_products, name='products')
+    path('', all_products, name='products'),
+    path('<int:id>', detail_product, name='product-detail'),
+    path('cat/<int:id>/', all_products, name='products-by-cat'),
 ]
--- core/product_module/views.py
+++ core/product_module/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 
 # Create your views here.
 
-def all_products(request):
+def all_products(request, id=None):
     products = Product.objects.all()
-    return render(request, 'product_module/products.html', context={'products': products})
+    category = Category.objects.all()
+    if id:
+        category = Category.objects.get(id=id)
+        products = Product.objects.filter(category=category)
+
+    return render(request, 'product_module/products.html', context={'products': products, 'category': category})
+
+
+def detail_product(request,id):
+    product = Product.objects.get(id=id)
+    return render(request,'product_module/product_detail.html',context={'product':product})
